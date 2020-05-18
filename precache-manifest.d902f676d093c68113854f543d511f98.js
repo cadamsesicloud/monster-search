30a8d905a03009989c1afa46cc76c44beb1bce3b
Charles Adams <cadams@esicloud.com>
2020-05-18 16:33:27 -0700

    Updates

---
+++ precache-manifest.d902f676d093c68113854f543d511f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0933f822f3b0bc8fd67f0f1748ad6fd8",
+    "url": "/monsters-search/index.html"
+  },
+  {
+    "revision": "aede550e2d8fd38a7a61",
+    "url": "/monsters-search/static/css/main.6e2909e3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-search/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-search/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aede550e2d8fd38a7a61",
+    "url": "/monsters-search/static/js/main.0e43ae75.chunk.js"
+  },
+  {
+    "revision": "4651df120e3072d692e0",
+    "url": "/monsters-search/static/js/runtime-main.03377779.js"
+  }
+]);
